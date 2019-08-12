a9df7b3f6a126ccb2f75443da7cb7981fc32badf
Sofiya-Palianytsia <sofiya.palianytsia@gmail.com>
2019-08-12 09:51:10 +0000

    JSON

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
 document.querySelector('.message-form input[type=submit]')
-.addEventListener('click', submit);
+.addEventListener('click', login);
 
-function submit(ev) {
-    debug;
-    ev.preventDefault();
-    fetch ('submit', {
+function login(e) {
+    e.preventDefault();
+    fetch ('login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            name: document.querySelector('.message-form input[name=name]').value,
-            email: document.querySelector('.message-form input[name=email]').value,
-            message: document.querySelector('.message-form input[name=message]').value
-        })
+           name: document.querySelector('.message-form input[name=name]').value,
+           email: document.querySelector('.message-form input[name=email]').value,
+           message: document.querySelector('.message-form textarea[name=message]').value
+       })
     })
     .then( _ => document.querySelector('.message-form').reset());
 }
