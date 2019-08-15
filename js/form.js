b9cde7280b142c3c977700dcc94600149c59cd28
Sofiya-Palianytsia <sofiya.palianytsia@gmail.com>
2019-08-15 16:38:09 +0000

    css finish

---
+++ js/form.js
@@ -0,0 +1,18 @@
+document.querySelector('.message-form input[type=submit]')
+.addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+    fetch ('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+           name: document.querySelector('.message-form input[name=name]').value,
+           email: document.querySelector('.message-form input[name=email]').value,
+           message: document.querySelector('.message-form textarea[name=message]').value
+       })
+    })
+    .then( _ => document.querySelector('.message-form').reset());
+}
